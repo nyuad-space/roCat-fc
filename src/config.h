bdc4076e7a305a9de4366253e6598bdfe94c61d6
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2023-06-08 02:49:34 +0400

    Updated code

--- src/config.h
+++ src/config.h
@@ -3,8 +3,13 @@
 
 #include <Arduino.h>
 #include <SPI.h>
+#include <Wire.h>
 #include <RH_RF95.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_INA219.h>
+#include <ezBuzzer.h>
+#include <LPS.h>
+#include <BMI088.h>
 
 #define RF95_FREQ 915.0
 
@@ -14,12 +19,13 @@
 
 #define LED_PIN PF10      // PF10
 #define NUM_LEDS 1
-Adafruit_NeoPixel LED(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#define CUR_I2C_SDA 10 // PF0
-#define CUR_I2C_SCL 11 // PF1
+#define CUR_I2C_SDA PF0 // PF0
+#define CUR_I2C_SCL PF1 // PF1
 
 #define HDRM4_DO 1 // PE2
 #define HDRM4_DI 2 // PE3
+
+#define BUZZER PD1
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "config.h"
 
-void setup() {
-  LED.begin();
-  LED.show();
+void setup()
+{
+
 }
 
-void loop() {
-  LED.setPixelColor(0, 64, 50, 64);
-  LED.show();
+void loop()
+{
+
 }
