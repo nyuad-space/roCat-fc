577ee679c0f23ff83c061f65e43a62d6df7df026
Euisun Choi <euisun2003@gmail.com>
2023-06-09 19:48:05 +0400

    Barometer test

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,38 @@
-#include "config.h"
-
-// SPIClass IMU_GYRO = SPIClass(IMU_SPI_MOSI, IMU_SPI_MISO, IMU_SPI_SCK);
-// SPIClass IMU_ACC = SPIClass(IMU_SPI_MOSI, IMU_SPI_MISO, IMU_SPI_SCK);
-// SPIClass LoRa_SPI = SPIClass(RF95_MOSI, RF95_MISO, RF95_SCK);
-// Adafruit_NeoPixel LED(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-// RH_RF95 rf95(RF95_CS, RF95_INT);
-// Adafruit_INA219 ina219;
-// ezBuzzer buzzer(BUZZER);
-// LPS lps;
-// Bmi088Gyro gyro(IMU_GYRO, PD13);
-// Bmi088Accel accel(IMU_ACC, PD9);
-// LoRaClass LORA_123;
+#include <config.h>
+#include <Adafruit_LPS2X.h>
+Adafruit_LPS22 barometer;
+SPIClass SPI_BARO;
 
 void setup()
 { 
   Serial.begin(115200);
+  //Stop if serial error
   while(!Serial)
   {
+    Serial.println("Serial error... Check wires and reload");
     delay(1);
   }
-  delay(1000);
-  
+  //Wait for setups
+  delay(100);
+  //Barometer setup
+  if (!barometer.begin_SPI(BARO_SPI_CS, BARO_SPI_SCK, BARO_SPI_MISO, BARO_SPI_MOSI))
+  {
+    Serial.println("Failed to find sensor");
+    while(1) 
+    {
+      delay(1);
+    }
+  }
+  Serial.println("Found sensor!");
 }
 
 void loop()
 {
-
+  sensors_event_t temp;
+  sensors_event_t presssure;
+  barometer.getEvent(&presssure, &temp);
+  Serial.print(temp.temperature);
+  Serial.print(",");
+  Serial.println(presssure.pressure);
+  delay(100);
 }
